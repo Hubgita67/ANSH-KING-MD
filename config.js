0061eb34df90ad94f9cb15992712ce18edb07d1e
Hubgita67 <159114095+Hubgita67@users.noreply.github.com>
2024-10-12 22:09:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-MONGODB: process.env.MONGODB || "enter mongodb url",
+SESSION_ID: process.env.SESSION_ID || "GQkSAJYB#nm1y65VletchE8L7Q1cbNOGz1P2HVQIb2nrzrdAi4So",
+MONGODB: process.env.MONGODB || "mongodb+srv://suhail:suhail@clustere.tuhzpez.mongodb.net/?retryWrites=true&w=majority",
 };
